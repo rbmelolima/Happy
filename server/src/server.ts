860fc5832489f95fb6c41fce2280b2a793a80ec8
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-10-14 19:43:00 -0300

    feat(server): conectando com o banco de dados

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import './database/connection';
 
 const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  return response.json({ message: "aloooo" });
+});
 
 app.listen(3333);
