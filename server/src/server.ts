665a13844bcacbbdb18513a7f50428a3a9389ecd
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-10-14 22:37:28 -0300

    feat(server): add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import './database/connection';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import path from 'path';
@@ -8,6 +9,7 @@ import errorHandler from './errors/handler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
