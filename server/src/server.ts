049c76f1a6b777f68a2c7094115af4ebfa8f69de
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-10-14 21:08:03 -0300

    feat(server): configurando o typescript para habilitar entities

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,39 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './model/Orphanage';
+
 import './database/connection';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ message: "aloooo" });
+app.post('/orphanages', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    instructions,
+    about,
+    opening_hours,
+    open_on_weekends
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return response.status(201).json(orphanage);
 });
 
-app.listen(3333);
+app.listen(3333, ()=> console.log("\n\n ** Server is running in localhost:3333 **\n\n"));
