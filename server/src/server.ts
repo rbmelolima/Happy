6c8e5a0e3eeaca3c7ef6d33563eac85214c047c1
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-10-14 22:23:13 -0300

    feat(server): lidando com exceções

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import './database/connection';
+import 'express-async-errors';
 
 import routes from './routes';
-
 import path from 'path';
+import errorHandler from './errors/handler';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(3333, () => console.log("\n\n ** Server is running in localhost:3333 **\n\n"));
